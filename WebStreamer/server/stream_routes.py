9f580238e1f6bfa765d1e589873999cfe854895e
DeekshithSH <shdeekshith@gmail.com>
2021-11-11 13:02:26 +0530

    make it better

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -15,22 +15,23 @@ routes = web.RouteTableDef()
 HTML_PAGE="""<!DOCTYPE html>
 <html>
    <body>
-      <p>Welcome to TeleXBot</p>
+      Status: Running
+      Maintained By: Deekshith SH
    </body>
 </html>"""
 
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response({"status": "running",
-                              "maintained_by": "Deekshith SH",
-                              "uptime": get_readable_time(time.time() - StartTime),
-                              "telegram_bot": '@'+(await StreamBot.get_me()).username})
-@routes.get('/home')
+@routes.get('/')
 async def index_handler(request):
     return web.Response(
         text=HTML_PAGE,
         content_type='text/html')
 
+@routes.get("/status", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response({"status": "running",
+                              "maintained_by": "Deekshith SH",
+                              "uptime": get_readable_time(time.time() - StartTime),
+                              "telegram_bot": '@'+(await StreamBot.get_me()).username})
 
 @routes.get("/{message_id}")
 async def stream_handler(request):
