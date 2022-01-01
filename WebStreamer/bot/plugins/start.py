a01c65634e9cafdb8e3572dc2c4259724de7ce04
DeekshithSH <shdeekshith@gmail.com>
2022-01-01 23:39:38 +0530

    Nothing

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -64,7 +64,7 @@ ABOUT_BUTTONS = InlineKeyboardMarkup(
 async def cb_data(bot, update):
     if update.data == "home":
         await update.message.edit_text(
-            text=START_TEXT.format(update.from_user.mention),
+            text=START_TEXT.format(update.from_user.mention, await db.total_users_count()),
             disable_web_page_preview=True,
             reply_markup=START_BUTTONS
         )
