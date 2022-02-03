6c676075d73313861ad9c8659bfa773e21fea25a
DeekshithSH <shdeekshith@gmail.com>
2022-02-03 15:57:00 +0530

    status to server

--- WebStreamer/bot/plugins/status.py
+++ WebStreamer/bot/plugins/status.py
@@ -5,7 +5,7 @@ from WebStreamer import StartTime
 from WebStreamer.vars import Var
 
 
-@StreamBot.on_message(filters.private & filters.command("status") &  ~filters.edited)
+@StreamBot.on_message(filters.private & filters.command("server") &  ~filters.edited)
 async def stats(bot, update):
     currentTime = readable_time((time.time() - StartTime))
     total, used, free = shutil.disk_usage('.')
